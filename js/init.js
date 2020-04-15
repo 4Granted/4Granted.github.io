2478433d499100d4989cc6c5bf6080686115f930
RuthlessBoi <ruthlessbug13@gmail.com>
2020-04-14 19:33:17 -0500

    Updated some info and added a navbar.
    Updated some info, components and colors. Also added a navbar with a logo.

--- js/init.js
+++ js/init.js
@@ -1,33 +1,46 @@
 const discordId = "Ruthless#5405";
 
+(function ($) {
+    $(function () {
+        $('.sidenav').sidenav();
+
+        tippy('#portfolioSpy', {
+            content: "Hopefully soon",
+            arrow: true,
+            placement: 'bottom',
+            hideOnClick: false,
+        });
+        
+        tippy('#discordContactSpy', {
+            content: "Click to copy: " + discordId,
+            arrow: true,
+            placement: 'top',
+            hideOnClick: false,
+        });
+        
+        tippy('#discordSpy', {
+            content: "Click to copy",
+            arrow: true,
+            placement: 'top',
+            hideOnClick: false,
+        });
+        
+        tippy("#twitterSpy", {
+            content: "Heh, haven't used it ever",
+            arrow: true,
+            placement: 'top',
+            hideOnClick: false,
+        });
+        
+        tippy("#githubSpy", {
+            content: "All my beloved projects",
+            arrow: true,
+            placement: 'top',
+            hideOnClick: false,
+        });
+    });
+})(jQuery);
+
 function c2c() {
-     navigator.clipboard.writeText(discordId);
+    navigator.clipboard.writeText(discordId);
 }
-
-tippy('#discordContactSpy', {
-    content: "Click to copy: " + discordId,
-    arrow: true,
-    placement: 'top',
-    hideOnClick: false,
-});
-
-tippy('#discordSpy', {
-    content: "Click to copy",
-    arrow: true,
-    placement: 'top',
-    hideOnClick: false,
-});
-
-tippy("#twitterSpy", {
-    content: "Heh, haven't used it ever.",
-    arrow: true,
-    placement: 'top',
-    hideOnClick: false,
-});
-
-tippy("#githubSpy", {
-    content: "All my beloved projects.",
-    arrow: true,
-    placement: 'top',
-    hideOnClick: false,
-});
